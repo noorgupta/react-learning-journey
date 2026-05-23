74ca91ed19220dfdc3a8a5109358812ef26a34fa
noorgupta <noorgupta24@navgurukul.org>
2026-05-23 18:00:49 +0530

    Completed Week 1 Day 2 JSX profile card

---
+++ Week1/react-app/src/App.jsx
@@ -0,0 +1,30 @@
+import heroImg  from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  const name = "Joy";
+  const course = "BCAA";
+  const city = "Jal";
+  const role = "Full-stck dev";
+  const hobby = "singing songs"
+
+  function getAge(){
+    return 20;
+  }
+
+  return (
+    <>
+      <div className='card'>
+      <img className="profile-img" src = {heroImg} alt="Profile"/>
+      <h1>Hello, I am { name } from { city }</h1>
+      <h2>My age is: {getAge()}</h2>
+      <h2>I am doing { course }</h2>
+      <h2>I am preparing for the { role }</h2>
+      <h2>Moreover, I like { hobby }</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
